0c20801fe5caac4d05b7ae1027c8ee1ddb01d490
Andy <azogg65@bluewin.ch>
2025-05-30 07:01:34 +0200

    add version.h file for build version management

---
+++ templates/version.h
@@ -0,0 +1,10 @@
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+
+// Automatically generated build version. Do not modify.
+#define VERSION_BUILD 0
+#define VERSION_DATE "2024-06-02"
+#define VERSION_TIME "13:13:52"
+#define VERSION "1.0.0.0"
+#define VERSION_STR "1.0.0.0 (2024-06-02 13:13:52)"
